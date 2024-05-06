5cee667313cf8e496a9b32f2003551138a365160
Jay-Gleijsteen <36629@ma-web.nl>
2024-05-06 11:51:12 +0200

    les 1

--- ConsoleMonGame/ConsoleMonGame/obj/Debug/net8.0/ConsoleMonGame.AssemblyInfo.cs
+++ ConsoleMonGame/ConsoleMonGame/obj/Debug/net8.0/ConsoleMonGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleMonGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d2d02bc7479f9e6cff5cd6f7f6f33dd7e6f83a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b1742f25e54a04f320104a1cd56ed7f4728201")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleMonGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleMonGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
